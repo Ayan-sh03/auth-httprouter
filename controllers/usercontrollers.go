b4d2d848b4658da2af9bfbdd25a883b9aa3db12a
Ayan Sheikh <114312039+Ayan-sh03@users.noreply.github.com>
2023-10-26 14:43:08 +0530

    added verified check in login

--- controllers/usercontrollers.go
+++ controllers/usercontrollers.go
@@ -65,7 +65,33 @@ func LoginController(w http.ResponseWriter, r *http.Request, q *db.Queries) {
 		respondWithError(w, http.StatusBadRequest, err.Error())
 		return
 	}
-	log.Println(user)
+	// log.Println(user)
+
+	if !user.IsVerified {
+		auth := smtp.PlainAuth("", os.Getenv("EMAIL"), os.Getenv("PASSWORD"), "smtp.gmail.com")
+		otp := user.Otp
+
+		to := []string{user.Email}
+
+		message := []byte("To: " + user.Email + "\r\n" +
+			"Subject: OTP for Registration\r\n" +
+			"MIME-Version: 1.0\r\n" +
+			"Content-Type: text/html; charset=\"utf-8\"\r\n\r\n" +
+			"<html><body>" +
+			"<h1>Your OTP for registration is <strong>" + otp.String + "</strong></h1>" +
+			"</body></html>")
+
+		go func() {
+			err := smtp.SendMail("smtp.gmail.com:587", auth, os.Getenv("EMAIL"), to, message)
+			if err != nil {
+				log.Println("Error in sending OTP:", err)
+			}
+		}()
+		//!
+
+		respondWithError(w, http.StatusUnauthorized, "User is not verified ,  please verify Otp Sent ")
+		return
+	}
 
 	credentialsError := models.CheckPassword(request.Password, user.Password)
 	if credentialsError != nil {
